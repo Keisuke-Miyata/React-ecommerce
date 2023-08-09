09702ed2344158d82b5374b276232aa6e5756452
Cypher0007 <miyatakeisuke@miyatakeisukes-MacBook-Air.local>
2023-08-09 11:41:40 +0200

    my first react project

---
+++ src/products.js
@@ -0,0 +1,25 @@
+import product1 from './assets/iphone.png'
+import product2 from './assets/camera.png'
+import product3 from './assets/cable.png'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 999.0,
+        productImage: product1, 
+    },
+    {
+        id: 2,
+        productName: "Camera",
+        price: 455.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Cable",
+        price: 10.5,
+        productImage: product3,
+    }
+
+]
